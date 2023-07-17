acbef9d69b9a1fb0e71332fddaaa1d6483c4f528
Gabriel <gabriel.ifsal@gmail.com>
2023-07-17 04:42:11 -0300

    Removing the socket.io dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { FormEvent, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { io } from "socket.io-client";
 const socket = io("http://localhost:3333");
